1ed2a10214b81f8e84232ecbe95a28132b302e48
joaoguiaguiar <joaoguiaguiar05@gmail.com>
2025-05-23 11:48:51 -0300

    atualizadno function.php

--- wp-content/themes/blocksy-child/functions.php
+++ wp-content/themes/blocksy-child/functions.php
@@ -94,3 +94,48 @@ function mi_add_inline_scripts() {
 
 
 
+function meu_script_overlay_postx() {
+  ?>
+<script>
+    jQuery(document).ready(function($) {
+      $('.ultp-block-item').each(function() {
+        var $item = $(this);
+        var $imageContainer = $item.find('.ultp-block-image');
+        var $image = $imageContainer.find('img');
+        var $title = $item.find('.ultp-block-title');
+		var $link = $title.find('.ultp-component-simple'); 
+ 
+		  
+        if ($imageContainer.length && $title.length) {
+          $image.css({
+            'opacity': 'o.4', 
+            'transition': 'opacity 0.3s ease'
+          });
+          $imageContainer.css('position', 'relative');
+          $title.appendTo($imageContainer);
+          $title.css({
+            'position': 'absolute',
+            'bottom': '10px',
+            'left': '10px',
+            'padding': '0.5rem',
+            'z-index': '10',
+            'max-width': '90%',
+            'background': 'transparent',
+            'text-decoration': 'none',
+            'transition': 'all 0.3s ease',
+          });
+          $title.hover(
+       function () {
+              $(this).css('border-bottom', '1px solid white');
+            },
+            function () {
+              $(this).css('border-bottom', '1px solid transparent');
+            }
+          );
+        }
+      });
+    });
+</script>
+<?php
+}
+add_action('wp_footer', 'meu_script_overlay_postx');
