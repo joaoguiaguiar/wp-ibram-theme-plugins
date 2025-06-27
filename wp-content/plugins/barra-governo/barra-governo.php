6479df18ee1fc791668c0f51dbe1f32e909d74de
joaoguiaguiar <joaoguiaguiar05@gmail.com>
2025-06-27 09:59:40 -0300

    ajuste no plugin gov

--- wp-content/plugins/barra-governo/barra-governo.php
+++ wp-content/plugins/barra-governo/barra-governo.php
@@ -1,23 +1,23 @@
 <?php
 /*
-Plugin Name: Barra do Governo Brasil
-Description: Adiciona a barra do governo ao tema pai sem ser removida em atualizações.
+Plugin Name: Barra do Governo
+Author: João Aguiar
+Author URI: https://github.com/joaoguiaguiar
+Description: Ao ativar o plugin, a Barra do Governo Federal é automaticamente exibida no topo do site, independentemente do tema utilizado, garantindo visibilidade e funcionamento contínuo.
 Version: 1.0
-Author: joao.aguiar
 */
 
 if (!defined('ABSPATH')) {
     exit;
 }
 
-function adicionar_barra_governo_tema_pai() {
+function adicionar_barra_governo() {
     echo '<div id="barra-brasil"></div>';
     echo '<script defer src="https://barra.brasil.gov.br/barra_2.0.js"></script>';
 }
+add_action('wp_body_open', 'adicionar_barra_governo');
 
-add_action('wp_body_open', 'adicionar_barra_governo_tema_pai');
-
-function adicionar_estilo_barra_governo() {
+function estilo_barra_governo() {
     echo '<style>
         #barra-brasil {
             position: relative;
@@ -29,4 +29,4 @@ function adicionar_estilo_barra_governo() {
         }
     </style>';
 }
-add_action('wp_head', 'adicionar_estilo_barra_governo');
+add_action('wp_head', 'estilo_barra_governo');
